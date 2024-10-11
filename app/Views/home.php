8d37898b40deca7a8ebbbfef48315e86410589d5
Imesha <dewmini.26dias@gmail.com>
2024-10-11 19:01:58 +0530

    home page

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,44 +1,157 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Home</title>
-</head>
+    <head>
+        <title>Home page</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+    </head>
 
-<body>
-    <h1>Welcome to the Home Page</h1>
-    
-    <?php
-    
-    global $conn;
+    <body>
+        <div class="navigation">
+        <div class="wrapper">
+            <nav class="navbar">
+                <img class="logo" src="../img/logo.png" >
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#about">Contact Us</a></li>
+                    <li><a href="#register">Sign up</a></li>
+                    <li><a href="#######">Log in</a></li>
+                
+                </ul>
+                
+            </nav>
+        </div>
 
-    // Fetch travelers data
-    $sql = "SELECT FirstName, Email FROM traveler";
-    $result = $conn->query($sql);
+<!-- home-main -->
+        <div class="main">
+            <div>
+                <h2 class="welcome">Find the best places to stay, eat and explore  in <br>Sri Lanka</h2>
+                <p class="description">ExploreEase is a platform that helps you to find the best places to stay, eat <br>and explore in Sri Lanka.
+                 We provide you with the best hotels, resturants,<br>cultural events and heritage sites in Sri Lanka.</p>
+                <button >Explore</button>
+            </div>
+            <div>
+                <img src="../img/new.jpg" style="width:35vw;  height:80vh; padding-left: 200px; margin-top:120px">
+            </div>
+            <!-- <h1 class="welcome">Welcome to ExploreEase</h1>
+             <h3 class="description">Make your journey easier on our platform....</h3> 
+            <h3 class="description">Find the best places to stay, eat and explore in Sri Lanka</h3> 
+            <p>ExploreEase is a platform that helps you to find the best places to stay, eat and explore in Sri Lanka. </p><p>We provide you with the best hotels, resturants, cultural events and heritage sites in Sri Lanka. We also provide you with the best deals and discounts for your journey.</p>-->
+        </div>
+    </div>
 
-    $travelers = [];
-    if ($result->num_rows > 0) {
-        // Output data of each row
-        while($row = $result->fetch_assoc()) {
-            $travelers[] = $row;
-        }
-    } else {
-        echo "0 results";
-    }
+<!-- features -->
+    <section id="services-features" class="services-features">
 
-    $conn->close();
-    ?>
+        <h3  class="services-title">Our Services</h3>
+        
+        <div class="services">
 
-    <h2>Travelers</h2>
-    <ul>
-        <?php foreach ($travelers as $traveler): ?>
-            <li><?php echo $traveler['FirstName'] . ' - ' . $traveler['Email']; ?></li>
-        <?php endforeach; ?>
-    </ul>
+            <div class="serve-image">
+                <img src="" alt="image here">
+            </div>
 
-        <a href="login">Login</a>
+            <div class="serives">
 
-    <p>Don't have an account?</p>
-        <a href="signup">Signup</a>
-</body>
+            <div class="service-one">
+                <div class="service">
+                    <img src="../img/hotel.jpg" alt="hotels">
+                    <h3>Hotels</h3>
+                    <p>Find the best hotels in Sri Lanka</p>
+                </div>
 
+                <div class="service">
+                    <img src="../img/download (2).jpeg" alt="resturants">
+                    <h3>Resturants</h3>
+                    <p>Find the best resturants in Sri Lanka</p>
+                </div>
+            </div>
+
+
+            <div class="service-two">
+                <div class="service">
+                    <img src="../img/event.jpg" alt="cultural events">
+                    <h3>Cultural Events</h3>
+                    <p>Find the best cultural events in Sri Lanka</p>
+                </div>
+
+                <div class="service">
+                    <img src="../img/download (1).jpeg" alt="heritage">
+                    <h3>Heritage</h3>
+                    <p>Find the best heritage sites in Sri Lanka</p>
+                </div>
+                </div>
+            </div>
+
+
+            <div class="service-features">
+                <h2  class="features-title">Our Features</h2>
+
+
+                <div class="features">
+                <div class="fea-1">
+
+                    <div>
+                    <img src="../img/download (2).jpeg" alt="location icon">
+                    </div>
+
+                    <div class="details">
+                    <h3>Search by Location</h3>
+                    <p>You can get information about hotels, resturants, guides, transport service details and more suitable recommendations you want to know.</p>
+                    <button>explore</button>
+                    </div>
+
+                </div>
+
+                <div class="fea-2">
+                    <div class="details">
+                    <h3>Search by Key-Word</h3>
+                    <p>This feature allows you to get recommendations according to the selected key words</p>
+                    <button>explore</button>
+                    </div>
+
+                    <div>
+                    <img src="../img/image.jpg" alt="map icon">
+                    </div>
+                </div>
+
+                </div>
+            </div>
+        </div>
+     </section>
+<!-- register-->
+<section class="register" id="register">
+            <div class="sign-up" >
+                <h4></h4>
+            <div>
+                <img src="#">
+                <h2>Register with us as a Traveller</h2>
+                <p>Register with us. We make your journey more 
+                    comfortable in our platform.</p>
+                <button>Register</button>
+            </div>
+
+            <div>
+                <img src="#">
+                <h2>Register with us as a Service Provider</h2>
+                <p>Register with us to promote your services and 
+                    We connect you and travellers through our platform.</p>
+                <button>Register</button>
+            </div>
+            </div>
+        </section>
+
+<!-- about us -->
+        <div class="about-us">
+
+        </div>
+
+<!-- contact us -->
+        <div class="contact-us">
+
+        </div>
+    </body>
 </html>
