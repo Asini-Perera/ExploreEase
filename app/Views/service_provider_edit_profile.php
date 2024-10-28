fb5c0828c20d676b677699a37105d89064420065
Imesha <dewmini.26dias@gmail.com>
2024-10-28 19:09:27 +0530

    updated code

---
+++ app/Views/service_provider_edit_profile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Service Provider Edit Profile</title>
+        <link rel="stylesheet" type="text/css" href="#####">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+    </head>
+
+    <body>
+        <div class="navigation">
+        <div class="wrapper">
+            <nav class="navbar">
+                <img class="logo" src="../img/logo.png" >
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#about">Contact Us</a></li>
+                    <li><a href="#register">Sign up</a></li>
+                    <li><a href="#######">Log in</a></li>
+                
+                </ul>
+                
+            </nav>
+        </div>
+    </body>
+</html>
