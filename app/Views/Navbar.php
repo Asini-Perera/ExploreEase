e770011f153faf9be97886740c5aa3ba082e9c27
asini <asiniperera01gmail.com>
2024-11-24 17:30:26 +0530

    Footer ad navbar fixed

---
+++ app/Views/Navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar.php -->
+<nav class="nav-container" aria-label="Main Navigation">
+    <ul class="menu-items">
+        <!-- Logo as a list item -->
+        <li class="menu-item logo">
+            <a href="#home">
+                <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/b864e43f0e40c7710ad7e60a0cb8ca31709d797ffc7d234befdf0016bb5d211e?placeholderIfAbsent=true&apiKey=133f3dae0e9c43f59e9b763518a0651f" alt="Logo" class="logo-background">
+            </a>
+        </li>
+        <li class="menu-item home"><a href="#home">Home</a></li>
+        <li class="menu-item about-us"><a href="#about">About Us</a></li>
+        <li class="menu-item contact-us"><a href="#contact">Contact Us</a></li>
+        <li class="menu-item services"><a href="#services">Services</a></li>
+        <li class="menu-item user">
+            <a href="#user">
+                 <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/c6e57bb49f95aa90ea03c94c662f8934a585a46bc433e82d0060c589915ab3de?placeholderIfAbsent=true&apiKey=133f3dae0e9c43f59e9b763518a0651f" alt="Social Media Icon" class="social-icon" />
+            </a>
+        </li>
+    </ul>
+</nav>
