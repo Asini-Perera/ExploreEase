805d6df52d018fb3dde8bac8d0f3272b1ebfb20f
Imesha <dewmini.26dias@gmail.com>
2024-12-01 03:47:53 +0530

    add , edit posts

--- app/Views/restaurant_dashboard/edit_post.php
+++ app/Views/restaurant_dashboard/edit_post.php
@@ -1,21 +1,18 @@
 <link rel="stylesheet" href="../public/css/restaurant_dashboard/edit_post.css">
 
 <div class="form-content">
-    <h1>Add New Post</h1>
+    <h1>Edit Post</h1>
     
     <form method="post" action="">
         <div class="form-group">
-            <label for="title">Title:</label>
-            <input type="text" name="title" id="title" class="form-control" placeholder="Enter food name" required>
+            <input type="text" name="title" id="title" class="form-control" placeholder="Enter title" required>
         </div>
 
         <div class="form-group">
-            <label for="price">Description:</label>
-            <textarea id="description" cols="60" rows="40" class="form-control" placeholder="Enter price" required></textarea>
+            <textarea id="description" cols="60" rows="40" class="form-control" placeholder="Enter Description" required></textarea>
         </div>
         
         <div class="form-group">
-            <label for="image">Image:</label>
             <input type="file" name="image" id="image" class="form-control">
         </div>
 
