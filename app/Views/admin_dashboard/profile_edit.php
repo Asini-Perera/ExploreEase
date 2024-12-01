50865af467f20e762c499388e7b23cd5d77fe776
Uthum Imasha <imashauthum@gmail.com>
2024-12-01 20:00:08 +0530

    Refactor profile image handling and enforce required fields in signup form

--- app/Views/admin_dashboard/profile_edit.php
+++ app/Views/admin_dashboard/profile_edit.php
@@ -33,13 +33,7 @@
     </form>
 </div>
 
-<script>
-    function previewImage(event) {
-        const reader = new FileReader();
-        reader.onload = function() {
-            const output = document.getElementById('currentProfileImage');
-            output.src = reader.result;
-        };
-        reader.readAsDataURL(event.target.files[0]);
-    }
-</script>
+<script src="../public/js/image_preview.js"></script>
+
+
+    
