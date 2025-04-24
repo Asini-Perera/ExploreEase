e501d67e1ebe80a02373dfa69284695d1399e4b3
Uthum Imasha <imashauthum@gmail.com>
2025-04-24 15:50:32 +0530

    Refactor restaurant view to dynamically display restaurant details and improve layout; enhance HomeModel with keyword filtering and retrieval methods for hotels and restaurants; update routing to include a new service link.

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,7 @@ $router->add('service/menu', 'HomeController@travelerside_menu');  // traveller 
 $router->add('reviews', 'HomeController@siteReview');  // Site reviews page
 $router->add('review/save', 'HomeController@saveReview');  // Save review process
 $router->add('filter/keyword', 'HomeController@filterKeyword');  // Filter by keyword process
+$router->add('link/service', 'HomeController@linkService');  // Link service page for displaying
 
 $router->add('keyword', 'KeywordController@loadKeywordPage'); // Keyword page for users
 $router->add('keyword/save', 'KeywordController@saveKeywords');  // Save keywords for users
